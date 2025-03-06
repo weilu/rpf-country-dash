35990563438c48be30a56e20026d480b7a6bdff5
bhupatiraju <32596436+bhupatiraju@users.noreply.github.com>
2025-03-07 01:40:00 +0530

    Changed source to the UC catalog for queries

--- queries.py
+++ queries.py
@@ -7,8 +7,8 @@ HTTP_PATH = os.getenv("HTTP_PATH")
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 PUBLIC_ONLY = os.getenv("PUBLIC_ONLY", "False").lower() in ("true", "1", "yes")
 
-class QueryService:
 
+class QueryService:
     _instance = None
 
     @staticmethod
@@ -22,7 +22,7 @@ class QueryService:
         if PUBLIC_ONLY:
             query = """
                 SELECT country_name
-                FROM boost.data_availability
+                FROM prd_mega.boost.data_availability
                 WHERE boost_public = 'Yes'
             """
             self.country_whitelist = self.execute_query(query)["country_name"].tolist()
@@ -48,30 +48,32 @@ class QueryService:
         return self._apply_country_whitelist_filter(df)
 
     def _apply_country_whitelist_filter(self, df):
-        if self.country_whitelist is not None and 'country_name' in df.columns:
-            return df[df['country_name'].isin(self.country_whitelist)]
+        if self.country_whitelist is not None and "country_name" in df.columns:
+            return df[df["country_name"].isin(self.country_whitelist)]
         return df
 
     def get_expenditure_w_poverty_by_country_year(self):
         query = """
             SELECT *
-            FROM boost.pov_expenditure_by_country_year
+            FROM prd_mega.boost.pov_expenditure_by_country_year
         """
         df = self.fetch_data(query)
-        df.loc[:, "decentralized_expenditure"] = df["decentralized_expenditure"].fillna(0)
+        df.loc[:, "decentralized_expenditure"] = df["decentralized_expenditure"].fillna(
+            0
+        )
         return df
 
     def get_edu_private_expenditure(self):
         query = """
             SELECT country_name, year, real_expenditure
-            FROM boost.edu_private_expenditure_by_country_year
+            FROM prd_mega.boost.edu_private_expenditure_by_country_year
         """
         return self.fetch_data(query)
 
     def get_hd_index(self, countries):
         country_list = "', '".join(countries)
         query = f"""
-            SELECT * FROM indicator.global_data_lab_hd_index
+            SELECT * FROM prd_mega.indicator.global_data_lab_hd_index
             WHERE country_name IN ('{country_list}')
             ORDER BY country_name, year
         """
@@ -79,20 +81,20 @@ class QueryService:
 
     def get_learning_poverty_rate(self):
         query = """
-            SELECT * FROM indicator.learning_poverty_rate
+            SELECT * FROM prd_mega.indicator.learning_poverty_rate
         """
         return self.fetch_data(query)
 
     def get_expenditure_by_country_func_econ_year(self):
         query = """
-            SELECT * FROM boost.expenditure_by_country_func_econ_year
+            SELECT * FROM prd_mega.boost.expenditure_by_country_func_econ_year
         """
         return self.fetch_data(query)
 
     def get_expenditure_by_country_sub_func_year(self):
         query = """
             SELECT country_name, admin0, year, func, latest_year, func_sub, expenditure, real_expenditure
-            FROM boost.expenditure_by_country_admin0_func_sub_year
+            FROM prd_mega.boost.expenditure_by_country_admin0_func_sub_year
         """
         return self.fetch_data(query)
 
@@ -100,7 +102,7 @@ class QueryService:
         country_list = "', '".join(countries)
         query = f"""
             SELECT country_name, display_lon, display_lat, zoom, income_level
-            FROM indicator.country
+            FROM prd_mega.indicator.country
             WHERE country_name IN ('{country_list}')
         """
         return self.fetch_data(query)
@@ -108,7 +110,7 @@ class QueryService:
     def get_expenditure_by_country_geo1_year(self):
         query = """
             SELECT country_name, year, adm1_name, expenditure, per_capita_expenditure
-            FROM boost.expenditure_by_country_geo1_year
+            FROM prd_mega.boost.expenditure_by_country_geo1_year
         """
         return self.fetch_data(query)
 
@@ -116,7 +118,7 @@ class QueryService:
         country_list = "', '".join(countries)
         query = f"""
             SELECT country_name, admin1_region, boundary
-            FROM indicator.admin1_boundaries_gold
+            FROM prd_mega.indicator.admin1_boundaries_gold
             WHERE country_name IN ('{country_list}')
             ORDER BY country_name
         """
@@ -125,34 +127,34 @@ class QueryService:
     def get_subnational_poverty_index(self, countries):
         country_list = "', '".join(countries)
         query = f"""
-            SELECT * FROM indicator.subnational_poverty_index
+            SELECT * FROM prd_mega.indicator.subnational_poverty_index
             WHERE country_name IN ('{country_list}')
         """
         return self.fetch_data(query)
 
     def get_universal_health_coverage_index(self):
         query = """
-            SELECT * FROM indicator.universal_health_coverage_index_gho
+            SELECT * FROM prd_mega.indicator.universal_health_coverage_index_gho
         """
         return self.fetch_data(query)
 
     def get_health_private_expenditure(self):
         query = """
             SELECT country_name, year, real_expenditure
-            FROM boost.health_private_expenditure_by_country_year
+            FROM prd_mega.boost.health_private_expenditure_by_country_year
         """
         return self.fetch_data(query)
 
     def expenditure_and_outcome_by_country_geo1_func_year(self):
         query = """
-            SELECT * FROM boost.expenditure_and_outcome_by_country_geo1_func_year
+            SELECT * FROM prd_mega.boost.expenditure_and_outcome_by_country_geo1_func_year
         """
         return self.fetch_data(query)
 
     def get_pefa(self, countries):
         country_list = "', '".join(countries)
         query = f"""
-            SELECT * FROM indicator.pefa_by_pillar
+            SELECT * FROM prd_mega.indicator.pefa_by_pillar
             WHERE country_name IN ('{country_list}')
             ORDER BY country_name, year
         """
